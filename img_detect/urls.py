bdcb30e9a44765ca4c4faf62cfd33e1d0a7a8b0c
nmatsu <n.matsumura2022@gmail.com>
2024-01-14 08:47:31 +0000

    mod: img_detect APIのひな型

--- img_detect/urls.py
+++ img_detect/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ImgDetectViewSet
 
 router = DefaultRouter()
-router.register('imgDetect', ImgDetectViewSet)
+router.register('img_detect', ImgDetectViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
